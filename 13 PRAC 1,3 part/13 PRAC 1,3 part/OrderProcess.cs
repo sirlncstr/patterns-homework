404f9f4db3301e972dc735df41b5d5e01f8789fd
sirlncstr <caneuslucky@gmail.com>
2025-12-06 22:58:40 +0500

    13PRAC

---
+++ 13 PRAC 1,3 part/13 PRAC 1,3 part/OrderProcess.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class OrderProcess
+{
+    public void SelectProducts()
+    {
+        Console.WriteLine("Выбор товаров и добавление в корзину.");
+    }
+
+    public void FillOrder()
+    {
+        Console.WriteLine("Оформление заказа: ввод данных покупателя.");
+    }
+
+    public bool PayOrder()
+    {
+        Console.WriteLine("Оплата заказа.");
+        bool paymentOk = true;
+        return paymentOk;
+    }
+
+    public void ProcessOrder()
+    {
+        Console.WriteLine("Обработка заказа на складе.");
+    }
+
+    public void ShipOrder()
+    {
+        Console.WriteLine("Отправка заказа покупателю.");
+    }
+
+    public void CancelOrder()
+    {
+        Console.WriteLine("Заказ отменён.");
+    }
+}
---
+++ 13 PRAC 1,3 part/13 PRAC 1,3 part/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var process = new OrderProcess();
+
+        process.SelectProducts();
+        process.FillOrder();
+
+        bool paid = process.PayOrder();
+
+        if (paid)
+        {
+            process.ProcessOrder();
+            process.ShipOrder();
+        }
+        else
+        {
+            process.CancelOrder();
+        }
+    }
+}
